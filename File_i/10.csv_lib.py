e00369416a001fdd2a3925f3150b9cad6e6eb452
mukul <mukulkukreja009@gmail.com>
2026-02-27 16:55:18 +0530

    learn csv reader

--- File_i/10.csv_lib.py
+++ File_i/10.csv_lib.py
@@ -8,3 +8,15 @@ with open("File_i/10.csv_lib.csv") as file:
 for student in sorted(students, key=lambda student: student["name"]):
   print(f"{student['name']} is in {student['home']}")
 
+# if we face the problem of storing data in csv that had 3 commas but you read 2 in it 
+# then use csv library for it 
+import csv
+
+studentsp = []
+with open("File_i/10.csv_lib.csv") as filep:
+  reader = csv.reader(filep)
+  for name0, home0 in reader:
+    studentsp.append({"name": name0, "home": home0})
+
+for student in sorted(studentsp, key=lambda student: student["name"]):
+  print(f"{student['name']} is in {student['home']}")
