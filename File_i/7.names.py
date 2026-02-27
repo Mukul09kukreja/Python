91d7e5177fafbd19a50979f49533ecd8047776ce
mukul <mukulkukreja009@gmail.com>
2026-02-27 15:26:39 +0530

    done with reading and writing file now i am gonna start learning csv file

---
+++ File_i/7.names.py
@@ -0,0 +1,6 @@
+# So If i want to add more than than names in txt file
+# How to do it? 
+# Ans: So here we discuss about this how to store and use
+#      So here we use csv file instead of txt file to store data like excel or spreadshets
+
+# First we gonna read a csv file:
