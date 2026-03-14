290d6cfd4c1ab040a65fd18460bff851a6a926f6
mukul <mukulkukreja009@gmail.com>
2026-03-14 14:22:41 +0530

    learned groups in re lib

--- regular_expressions/11.hello.py
+++ regular_expressions/11.hello.py
@@ -1,3 +1,13 @@
 # Input: malan@harvard.cs50.edu
 # Output: Invalid 
 # Reason: because . symbol after @ 
+import re
+
+email = input("What's your email? ").strip()
+
+if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
+  print("Valid")
+else:
+  print("Invalid")
+
+# using (...)? here to solve the above problem ? is in 5 no file
