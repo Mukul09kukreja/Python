60ab6b5bd26e5d2d41a3487cbd20437469560865
mukul <mukulkukreja009@gmail.com>
2026-03-14 17:57:58 +0530

    learned walrus operator

--- regular_expressions/15.format.py
+++ regular_expressions/15.format.py
@@ -1,8 +1,12 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), *(.+)$", name)
-if matches:
+if matches := re.search(r"^(.+), *(.+)$", name):
   name = matches.group(2) + " " + matches.group(1)
 
 print(f"hello, {name}")
+''' := it is walrus operator it is new in python 
+  and it both allow you to assign a value as i'm doing 
+  from right to left.
+  and ask a boolean question also
+'''
