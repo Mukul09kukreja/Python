43cf8bf94ecf68bb3d14238c49b6539a2c1ab753
mukul <mukulkukreja009@gmail.com>
2026-03-16 22:50:21 +0530

    format the structure according to question

--- problem_Set_4/bitcoin_api/bitcoin_index.py
+++ problem_Set_4/bitcoin_api/bitcoin_index.py
@@ -1,9 +1,14 @@
 import requests
+while True:
+  try:
+    api = "96665cb5e9578163e96784373a8ca0c30050c7167baf77bb13992c50f1c51b46"
 
-api = "96665cb5e9578163e96784373a8ca0c30050c7167baf77bb13992c50f1c51b46"
+    url = f"https://rest.coincap.io/v3/assets/bitcoin?apiKey={api}"
 
-url = f"https://rest.coincap.io/v3/assets/bitcoin?apiKey={api}"
+    response = requests.get(url)
+    data = response.json()
 
-response = requests.get(url)
-data = response.json()
-print(data)
+    print(data)
+    break
+  except requests.RequestException:
+    continue
