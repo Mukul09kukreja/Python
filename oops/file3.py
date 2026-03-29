25fb1b52bceeaaa62af73ec61d7c02cba1d8b98f
mukul <mukulkukreja009@gmail.com>
2026-03-29 13:00:48 +0530

    learned conversion of tuple to list

--- oops/file3.py
+++ oops/file3.py
@@ -1,13 +1,16 @@
+# if you want to change something in tuple make it first in list then change what you want then convert it into tuple
 def main():
   student = get_student()
-  if student[0] == "Padma": # This gives an type error
+  
+  if student[0] == "Padma": 
     student[1] == "Ravenclaw"
+    
   print(f"{student[0]} from {student[1]}")
 
 def get_student():
   name = input("Name: ")
   house = input("House: ")
-  return (name, house) 
+  return [name, house] # change tuple into list
 
 if __name__ == "__main__":
   main()
